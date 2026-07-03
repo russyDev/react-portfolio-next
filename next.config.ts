d4f10635693b979b2bf43685fbfe8b9fbd4430f0
Ruslan Kolibabchuk <r.kolibabchuk@svitla.com>
2026-07-03 23:31:10 +0300

    refactor: simplify next.config.ts by removing unused configuration

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,5 @@
-// import type { NextConfig } from "next";
-//
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//
-//     /*output: 'export',*/
-//     trailingSlash: true,
-//     images: {
-//         unoptimized: true,
-//     },
-// };
-//
-// export default nextConfig;
-
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-    turbopack: {
-        rules: {
-            '*.svg': {
-                loaders: ['@svgr/webpack'],
-                as: '*.js',
-            },
-        },
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
